a96e66cb8eee599d1361a6a294e4843a7bd2176a
hwq <1305934019@qq.com>
2021-10-20 19:52:36 +0800

    wsda

--- DateStructureExeprience/Exeprience1/Node.h
+++ DateStructureExeprience/Exeprience1/Node.h
@@ -6,6 +6,17 @@ struct Node
 ElemType data; // 数据域
 Node<ElemType> *next; // 指针域
 // 构造函数:
-Node(); // 无参数的构造函数
-Node(ElemType e, Node<ElemType> *link = NULL); // 已知数数据元素值和指针建立结构
+Node(){} // 无参数的构造函数
+Node(ElemType e, Node<ElemType> *link = NULL){
+    if(link==NULL){
+        cout<<"error";
+        }
+    else{
+        link->data=e;
+    }
+}// 已知数数据元素值和指针建立结构
+*Node newNode(ElemType e){
+    Node *head=new Node;
+    head->data=e;
+} 
 };
