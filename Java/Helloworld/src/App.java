00804f2f5495ebb03b89acd0d8028fd2642060da
hwq <1305934019@qq.com>
2021-10-21 15:05:41 +0800

    add java

---
+++ Java/Helloworld/src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("hello world");
+        
+    }
+}
+class trangle{
+    double length;
+    double width;
+    double size(){
+        return this.length*this.width;
+    }
+}
