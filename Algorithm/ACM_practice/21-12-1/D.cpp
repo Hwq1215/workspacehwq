67d346629ff455337c43e243473b62ea25df793b
hwq <1305934019@qq.com>
2021-12-11 19:51:26 +0800

    E finished

---
+++ Algorithm/ACM_practice/21-12-1/D.cpp
@@ -0,0 +1,13 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<cmath>
+using namespace std;
+typedef long long ll;
+int main(){
+    int n;
+    cin>>n;
+    std::vector<pair<int,ll>> content(n);
+    
+}
+
--- Algorithm/ACM_practice/21-12-1/E.cpp
+++ Algorithm/ACM_practice/21-12-1/E.cpp
@@ -9,15 +9,33 @@ using namespace std;
 int main(){
     int n;
     scanf("%d",&n);
-    int x,y;
-    for(x = 1;(x*(x+1))/2<n;x++){
-        for(y = x;(y*(y+1))/2<n;y++){
-            if((x*x + y*y + x + y)/2 == n){
-                cout<<"YES";
-                return 0;
+    bool flag = false;
+    int mid,l,r,A,B;
+    for(int i = 1;i<sqrt(2*n);i++){
+        A = i*(i+1)/2;
+        l = i,r = sqrt(2*n);
+        mid = (l + r)/2;
+        B = (mid*mid+mid)/2;
+        while(l<=r){
+            if(A + B > n){
+                r = mid-1;
+                mid = (r+l)/2;
             }
+            else if(A + B < n){
+                l = mid+1;
+                mid = (l+r)/2;
+            }
+            else{
+                flag = true;
+                break;
+            }
+            B = (mid*mid+mid)/2;
         }
+        if(flag) break;
     }    
+    if(flag)
+    cout<<"YES";
+    else 
     cout<<"NO";
     return 0;
 }
